c316f3bf19e931425cd299ecf860323b7828007c
yandutka@mail.ru <yandutka@mail.ru>
2022-03-19 18:16:06 +0300

    Add project files.

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(83, 363);
+            this.panel1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Ёлка";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Шестерня";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(0, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(83, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Дерево";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton2.Location = new System.Drawing.Point(0, 145);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 19);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Линия";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton1.Location = new System.Drawing.Point(0, 126);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 19);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Кисть";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.trackBar1.Location = new System.Drawing.Point(0, 81);
+            this.trackBar1.Maximum = 40;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(83, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Brown;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 40);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 41);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(492, 363);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 363);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Paint";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        //Создаём хост для рисования
+        Graphics graphics;
+        //Создаём кисть
+        SolidBrush brush, rightBrush;
+        //Переменные х и у нужны для запоминания положения курсора мышки во время нажатия
+        int x;
+        int y;
+        float secondAngle;  //Угол на который мы хотим отклонять наши ветки дерева
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //Привязываем PictureBox к хосту
+            graphics = pictureBox1.CreateGraphics();
+            //Присваиваем цвет и форму кисти, инициализация объекта кисти
+            brush = new SolidBrush(Color.Green); //SolidBrush - залитая сплошная кисть 
+            rightBrush = new SolidBrush(Color.Brown);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) //Рисование кистью
+        {
+            if (radioButton1.Checked)
+            {
+                //e - "указатель" на наш курсор мыши
+                if (e.Button == MouseButtons.Left)   //При нажатии ЛКМ:
+                    graphics.FillEllipse(brush, e.X, e.Y, trackBar1.Value, trackBar1.Value);  //Рисуется круг с координатами позиции курсора мышки  
+                if (e.Button == MouseButtons.Right) //При нажатии ПКМ:
+                    graphics.FillEllipse(rightBrush, e.X, e.Y, trackBar1.Value, trackBar1.Value); 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rightBrush.Color = colorDialog1.Color;
+                button2.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)   //Рисование ручкой линий
+        {
+            if (radioButton2.Checked)
+            {
+                Pen pen = new Pen(brush);
+                if (e.Button == MouseButtons.Left)
+                    pen = new Pen(brush, trackBar1.Value);
+                if (e.Button == MouseButtons.Right)
+                    pen = new Pen(rightBrush, trackBar1.Value);
+                
+                graphics.DrawLine(pen, x, y, e.X, e.Y); 
+            }
+        }
+
+        void DrawLine(float x, float y, float angle, int length)
+        {
+            if (length < 10)return;
+
+            float x1 = (float)Math.Cos(angle * (Math.PI / 180)) * length + x;
+            float y1 = (float)Math.Sin(angle * (Math.PI / 180)) * length + y;
+            Pen pen = new Pen(brush, trackBar1.Value - 3);
+            if (length < 40)
+                pen.Color = rightBrush.Color;
+            graphics.DrawLine(pen, x, y, x1, y1);
+
+            DrawLine(x1, y1, angle - secondAngle, length - 8);
+            DrawLine(x1, y1, angle + secondAngle, length - 8);
+            /*DrawLine(x1, y1, angle - secondAngle*2, length - 8);
+            DrawLine(x1, y1, angle + secondAngle*2, length - 8);*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            secondAngle = (float)Convert.ToDouble(textBox1.Text);
+            secondAngle *= -1;
+            DrawLine(pictureBox1.Width / 2, pictureBox1.Height - 40, -90, 100);
+        }
+
+        void DrawGearWheel(int count, int length)
+        {
+            float angle = -90;
+            float secondAngle = 360 / count;
+            int x = pictureBox1.Width / 2;
+            int y = pictureBox1.Height / 2;
+            for (int i = 0; i < count; i++)
+            {
+                float x1 = (float)Math.Cos(angle * (Math.PI / 180)) * length + x;
+                float y1 = (float)Math.Sin(angle * (Math.PI / 180)) * length + y;
+                Pen pen = new Pen(brush, trackBar1.Value);
+                graphics.DrawLine(pen, x, y, x1, y1);
+                angle += secondAngle;
+            }
+            graphics.FillEllipse(brush, x - 80, y - 80, 160, 160);
+            graphics.FillEllipse(new SolidBrush(Color.White), x - 40, y - 40, 80, 80);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            DrawGearWheel(Convert.ToInt32(textBox2.Text), 100);
+        }
+
+        void DrawPine()
+        {
+            Point point1 = new Point(250, 10);
+            Point point2 = new Point(270, 40);
+            Point point3 = new Point(230, 40);
+            Point point4 = new Point(250, 40);
+            Point point5 = new Point(285, 85);
+            Point point6 = new Point(215, 85);
+            Point point7 = new Point(250, 85);
+            Point point8 = new Point(303, 153);
+            Point point9 = new Point(197, 153);
+            Point point10 = new Point(250, 153);
+            Point point11 = new Point(330, 255);
+            Point point12 = new Point(170, 255);
+            Point[] polygonPoints1 = { point1, point2, point3 };
+            Point[] polygonPoints2 = { point4, point5, point6 };
+            Point[] polygonPoints3 = { point7, point8, point9 };
+            Point[] polygonPoints4 = { point10, point11, point12 };
+            
+            graphics.FillPolygon(brush, polygonPoints1);
+            graphics.FillPolygon(brush, polygonPoints2);
+            graphics.FillPolygon(brush, polygonPoints3);
+            graphics.FillPolygon(brush, polygonPoints4);
+            graphics.FillRectangle(rightBrush, 235, 255, 30, 30);
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //TODO: Кнопка отрисовывает ёлку
+            graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            DrawPine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                brush.Color = colorDialog1.Color;
+                button1.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
